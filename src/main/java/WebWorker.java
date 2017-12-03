d95d41adcaa642506168a88d10ae72fb111da793
Jean Marcellin <marcellinja@gmail.com>
2017-12-03 12:14:11 -0800

    - Added comments - changed variable names - and minor typos

--- src/main/java/WebWorker.java
+++ src/main/java/WebWorker.java
@@ -6,25 +6,35 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.swing.*;
 
+/**
+ * WebWorker class
+ */
 public class WebWorker extends Thread {
   private WebFrame frame;
   private String urlString;
   private int rowNum;
   private final String INTERRUPTED = "Interrupted";
 
+  /**
+   * Webworker constructor
+   * @param  url    [String with url of file]
+   * @param  rowNum [Number of rows]
+   * @param  frame  [Webframe object]
+   * @return        [WebWorker object]
+   */
   public WebWorker(String url,int rowNum, WebFrame frame) {
     urlString = url;
     this.frame = frame;
     this.rowNum = rowNum;
   }
 
+  /**
+   * Run implementation
+   */
   public void run(){
     frame.increaseThreads();
-    download();
-    frame.decreaseThreads();
-  }
 
-  private void download(){
+    System.out.println("Fetching...." + urlString);
     InputStream input = null;
     StringBuilder contents = null;
     try {
@@ -43,11 +53,13 @@ public class WebWorker extends Thread {
       contents = new StringBuilder(1000);
       long start = System.currentTimeMillis();
       while ((len = reader.read(array, 0, array.length)) > 0) {
+        System.out.println("Fetching...." + urlString + len);
         if(Thread.interrupted())
           frame.updateTable(rowNum, INTERRUPTED);
         contents.append(array, 0, len);
         Thread.sleep(100);
       }
+      System.out.print(contents.toString());
       long end = System.currentTimeMillis();
       SimpleDateFormat ft = new SimpleDateFormat ("hh:mm:ss");
       Date dat = new Date();
@@ -55,15 +67,18 @@ public class WebWorker extends Thread {
       String status = curTime + "   " + (end-start) + "ms   " + contents.length() + " bytes";
       frame.updateTable(rowNum, status);
     }
-  
+
     catch(MalformedURLException ignored) {
       frame.updateTable(rowNum, "err");
+      System.out.println("Exception: " + ignored.toString());
     }
     catch(InterruptedException exception) {
       frame.updateTable(rowNum, INTERRUPTED);
+      System.out.println("Exception: " + exception.toString());
     }
     catch(IOException ignored) {
       frame.updateTable(rowNum, "err");
+      System.out.println("Exception: " + ignored.toString());
     }
        // "finally" clause, to close the input stream
        // in any case
@@ -73,6 +88,8 @@ public class WebWorker extends Thread {
       }
       catch(IOException ignored) {}
     }
+
+    frame.decreaseThreads();
   }
 }
 
--- src/main/java/webFrame.java
+++ src/main/java/webFrame.java
@@ -11,104 +11,154 @@ import javax.swing.border.*;
 import javax.swing.table.*;
 
 
+/**
+ * WebFrame Class
+ */
 public class WebFrame extends JFrame {
-    
+
     private DefaultTableModel model;
     private JTable table;
     private JPanel panel;
-    private JButton single, concurrent, stop;
-    private JTextField field;
-    private JLabel running, completed, elapsed;
-    private JProgressBar progress;
+    private JButton singleBtn, concurrentBtn, stopBtn;
+    private JTextField textFld;
+    private JLabel runningLbl, completedLbl, elapsedLbl;
+    private JProgressBar progressBar;
     private Semaphore sem;
     private Thread t;
     private int completedWorks, runningThreads;
-    
+    private ArrayList<WebWorker> workers;
+
+    /**
+     * Constructor that sets Jframe and components
+     * @param  filename [Name of file]
+     * @return          [WebFrame object]
+     */
     public WebFrame(String filename){
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
 
         panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
         model = new DefaultTableModel(new String[] { "url", "status"}, 0);
+
         table = new JTable(model);
         table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 
         JScrollPane scrollpane = new JScrollPane(table);
         scrollpane.setPreferredSize(new Dimension(600,300));
         panel.add(scrollpane);
-        ReadFile(filename);
 
-        single = new JButton("Single Thread Fetch");
-        single.addActionListener(new SingleThread());
+        this.readFile(filename);
 
-        concurrent = new JButton("Concurrent Fetch");
-        concurrent.addActionListener(new Concurrent());
+        singleBtn = new JButton("Single Thread Fetch");
+        singleBtn.addActionListener(new SingleThread());
 
-        stop = new JButton("Stop");
-        stop.addActionListener(new StopAction());
-        stop.setEnabled(false);
+        concurrentBtn = new JButton("Concurrent Fetch");
+        concurrentBtn.addActionListener(new Concurrent());
 
-        field = new JTextField();
-        field.setMaximumSize(new Dimension(50,JTextField.HEIGHT));
+        stopBtn = new JButton("Stop");
+        stopBtn.addActionListener(new StopAction());
+        stopBtn.setEnabled(false);
 
-        running = new JLabel("Running: 0");
-        completed = new JLabel("Completed: 0");
-        elapsed = new JLabel("Elapsed: 0");
-        progress = new JProgressBar();
-        
+        textFld = new JTextField();
+        textFld.setMaximumSize(new Dimension(50,JTextField.HEIGHT));
+
+        runningLbl = new JLabel("Running: 0");
+        completedLbl = new JLabel("Completed: 0");
+        elapsedLbl = new JLabel("Elapsed: 0");
+        progressBar = new JProgressBar();
+
+
         panel.add(Box.createRigidArea(new Dimension(0, 5)));
-        panel.add(single); 
+        panel.add(singleBtn);
         panel.add(Box.createRigidArea(new Dimension(0, 5)));
-        panel.add(concurrent);
+        panel.add(concurrentBtn);
         panel.add(Box.createRigidArea(new Dimension(0, 5)));
-        panel.add(field);
+        panel.add(textFld);
         panel.add(Box.createRigidArea(new Dimension(0, 5)));
-        panel.add(running);
+        panel.add(runningLbl);
         panel.add(Box.createRigidArea(new Dimension(0, 5)));
-        panel.add(completed);
+        panel.add(completedLbl);
         panel.add(Box.createRigidArea(new Dimension(0, 5)));
-        panel.add(elapsed);
+        panel.add(elapsedLbl);
         panel.add(Box.createRigidArea(new Dimension(0, 5)));
-        panel.add(progress);
+        panel.add(progressBar);
         panel.add(Box.createRigidArea(new Dimension(0, 5)));
-        panel.add(stop);
-        
+        panel.add(stopBtn);
+
         this.add(panel);
         this.pack();
         this.setVisible(true);
     }
-    
+
+    /**
+     * Main Method
+     * @param args [None]
+     */
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        WebFrame frame = new WebFrame("link.txt");
+
+    }
+
+    /**
+     * Increase running thread count
+     */
     public void increaseThreads(){
         updateRunningLabel(runningThreads++);
     }
-    
+
+    /**
+     * Derecreases running thread count
+     */
     public synchronized void decreaseThreads(){
         updateRunningLabel(runningThreads--);
         sem.release();
     }
-    
-    private void increaseCompledted(){
+
+    /**
+     * Increases the completed text component
+     */
+    private void increaseCompleted(){
         completedWorks++;
-        completed.setText("Completed: " + completedWorks);
+        completedLbl.setText("Completed: " + completedWorks);
     }
-    
+
+    /**
+     * Updates the running thread label
+     * @param threadsRunning [number of running threads]
+     */
     private void updateRunningLabel(int threadsRunning){
-        running.setText("Running: " + threadsRunning);
+        runningLbl.setText("Running: " + threadsRunning);
     }
-    
+
+    /**
+     * Updates table
+     * @param rowNum [number of rows]
+     * @param str    [name of table component]
+     */
     public void updateTable(int rowNum,String str){
-        increaseCompledted();
-        progress.setValue(completedWorks);
+        increaseCompleted();
+        progressBar.setValue(completedWorks);
         model.setValueAt(str, rowNum, 1);
     }
-    
-    private void ReadFile(String filename) {
+
+    /**
+     * Read file helper method
+     * @param filename [name of file to be read]
+     */
+    private void readFile(String filename) {
         try {
-            BufferedReader buff = new BufferedReader(new FileReader("link.txt"));
+            BufferedReader buff = new BufferedReader(new FileReader(filename));
             try {
-                String line; 
+                String line;
                 while((line = buff.readLine()) != null){
                     model.addRow(new String[]{line, ""});
                 }
@@ -119,33 +169,33 @@ public class WebFrame extends JFrame {
             e.printStackTrace();
         }
     }
-    
-    public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            System.out.println(e);
-        } 
-    WebFrame w = new WebFrame("link.txt");
-    }
-    
+
+    /**
+     * Resets texts and values
+     */
     private void statusReset(){
         completedWorks = 0;
-        progress.setValue(0);
-        running.setText("Running: 0");
-        completed.setText("Completed: 0");
-        elapsed.setText("Elapsed: 0");
-        progress.setValue(0);
+        progressBar.setValue(0);
+        runningLbl.setText("Running: 0");
+        completedLbl.setText("Completed: 0");
+        elapsedLbl.setText("Elapsed: 0");
+        progressBar.setValue(0);
     }
-    
+
+    /**
+     * Start button implementation
+     */
     private void fetchButtonClicked(){
         statusReset();
-        stop.setEnabled(true);
-        single.setEnabled(false);
-        concurrent.setEnabled(false);
-        progress.setMaximum(model.getRowCount());
+        stopBtn.setEnabled(true);
+        singleBtn.setEnabled(false);
+        concurrentBtn.setEnabled(false);
+        progressBar.setMaximum(model.getRowCount());
     }
-    
+
+    /**
+     * Single thread implementation
+     */
     private class SingleThread implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -154,16 +204,22 @@ public class WebFrame extends JFrame {
             t.start();
         }
     }
-    
+
+    /**
+     * Concurrent thread implementation
+     */
     private class Concurrent implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
             fetchButtonClicked();
-            t = new Thread(new Launcher(Integer.parseInt(field.getText())));
+            t = new Thread(new Launcher(Integer.parseInt(textFld.getText())));
             t.start();
         }
     }
-    
+
+    /**
+     * Stop button implementation
+     */
     private class StopAction implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent arg0) {
@@ -171,19 +227,26 @@ public class WebFrame extends JFrame {
                 t.interrupt();
             t = null;
             statusReset();
-            single.setEnabled(true);
-            concurrent.setEnabled(true);
+            singleBtn.setEnabled(true);
+            concurrentBtn.setEnabled(true);
         }
     }
-    
-    ArrayList<WebWorker> workers;
+
+    /**
+     * Private inner class Launcher
+     */
     private class Launcher implements Runnable{
         private int workerLimit;
-        
+
+        /**
+         * Constructor
+         * @param  workerLimit [number of worker to be used]
+         * @return             [Launcher Object]
+         */
         public Launcher(int workerLimit) {
             this.workerLimit = workerLimit;
         }
-        
+
         @Override
         public void run() {
             long start = System.currentTimeMillis();
@@ -201,21 +264,21 @@ public class WebFrame extends JFrame {
                 workers.add(w);
                 w.start();
             }
-            
+
             for (int i = 0; i < workers.size(); i++) {
                 try {
                     workers.get(i).join();
                 } catch(InterruptedException e) {
-                    
+
                 }
             }
-            
+
             updateRunningLabel(runningThreads--);
             long end = System.currentTimeMillis();
-            elapsed.setText("Elapsed: " + (end-start));
-            stop.setEnabled(false);
-            single.setEnabled(true);
-            concurrent.setEnabled(true);
-        }  
+            elapsedLbl.setText("Elapsed: " + (end-start));
+            stopBtn.setEnabled(false);
+            singleBtn.setEnabled(true);
+            concurrentBtn.setEnabled(true);
+        }
     }
 }
